7660984dfcfc82906028ed622a2c2d19d7feec7a
Ayush Goel <ayush.goel@kiwitech.com>
2016-06-30 17:05:56 +0530

    Facebook Manager Ordering Updated.

--- SocialSharing/SocialSharing/PersistentDataManager.swift
+++ SocialSharing/SocialSharing/PersistentDataManager.swift
@@ -10,13 +10,14 @@ import UIKit
 
 class PersistentDataManager: NSObject {
 
-   static var socialListDictionary = ["FACEBOOK":["Login","Get Profile","Post text message in background",
-                                                "Post image and text message in background","Post Image and Text","Post Text Message",
-                                                "Post linked content","Post linked content in background","Get Friend",
-                                                "Invite Friend","Logout"],
-                                    "INSTAGRAM":["Login","Get User Profile","Get Friend's Profile","Get PublishedMedia","Logout"],
-                                    "TWITTER":["Login","Get Profile","Publish Post","Get All Post","Logout"],
-                                    "LINKEDIN":["Login","Get Profile","Publish Post","Logout"],
-                                    "GOOGLE":["Google-Login","Logout"]
+    static var socialListDictionary = ["FACEBOOK":["Login","Get Profile",
+        "Post image and text message in background","Post Image and Text",
+        "Post linked content","Post linked content in background","Get Friend",
+        "Invite Friend","Logout"],
+                                       "INSTAGRAM":["Login","Get User Profile","Get Friend's Profile","Get PublishedMedia","Logout"],
+                                       "TWITTER":["Login","Get Profile","Publish Post","Get All Post","Logout"],
+                                       "LINKEDIN":["Login","Get Profile","Publish Post","Logout"],
+                                       "GOOGLE":["Google-Login","Logout"]
     ]
+
 }
